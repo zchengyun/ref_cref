89321df3d409348b19f16ccb2fa8bd4708ea1c15
zchengyun <1690132420@qq.com>
2021-09-27 01:39:00 +0800

    添加项目文件。

---
+++ ref_cref.cpp
@@ -0,0 +1,20 @@
+﻿#include <functional>
+#include <iostream>
+
+void f(int& n1, int& n2, const int& n3)
+{
+    std::cout << "In function: n1[" << n1 << "]    n2[" << n2 << "]    n3[" << n3 << "]" << std::endl;
+    ++n1; // 增加存储于函数对象的 n1 副本
+    ++n2; // 增加 main() 的 n2
+    //++n3; // 编译错误
+    std::cout << "In function end: n1[" << n1 << "]     n2[" << n2 << "]     n3[" << n3 << "]" << std::endl;
+}
+
+int main()
+{
+    int n1 = 1, n2 = 1, n3 = 1;
+    std::cout << "Before function: n1[" << n1 << "]     n2[" << n2 << "]     n3[" << n3 << "]" << std::endl;
+    std::function<void()> bound_f = std::bind(f, n1, std::ref(n2), std::cref(n3));
+    bound_f();
+    std::cout << "After function: n1[" << n1 << "]     n2[" << n2 << "]     n3[" << n3 << "]" << std::endl;
+}
